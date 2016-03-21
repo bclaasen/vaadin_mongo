06c15ecd0a85a95824915bde68bb0a1d50690d4a
Bernd Daheim <bernd.claasen@gmx.net>
2016-03-22 00:37:42 +0100

    Goodrepo angebunden

---
+++ src/main/java/de/berndclaasen/datenmonster/frontend/good/GoodView.java
@@ -0,0 +1,10 @@
+package de.berndclaasen.datenmonster.frontend.good;
+
+import de.berndclaasen.datenmonster.frontend.generic.AbstractOverview;
+
+public class GoodView extends AbstractOverview {
+
+	public GoodView() {
+		super();
+	}
+}
