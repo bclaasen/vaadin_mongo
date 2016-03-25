2458fc4daee10da736c54d22c4de8adae747c7e4
Bernd Daheim <bernd.claasen@gmx.net>
2016-03-25 23:05:11 +0100

    aufgeräumt

--- src/main/java/de/berndclaasen/datenmonster/frontend/customer/CustomerOverviewPresenter.java
+++ src/main/java/de/berndclaasen/datenmonster/frontend/customer/CustomerOverviewPresenter.java
@@ -2,22 +2,14 @@ package de.berndclaasen.datenmonster.frontend.customer;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-import com.vaadin.ui.Label;
-import com.vaadin.ui.Notification;
-import com.vaadin.ui.PopupView;
-import com.vaadin.ui.UI;
-import com.vaadin.ui.VerticalLayout;
-import com.vaadin.ui.Window;
-
 import de.berndclaasen.datenmonster.backend.model.Customer;
 import de.berndclaasen.datenmonster.backend.service.SpringResolver;
 import de.berndclaasen.datenmonster.frontend.generic.AbstractOverviewPresenter;
 
 public class CustomerOverviewPresenter<V extends CustomerOverView, T extends Customer> extends AbstractOverviewPresenter<T,V> {
 
-	public CustomerOverviewPresenter(Class<V> clazz, Class<T> clazz2) {
-		super(clazz,clazz2);
-		
+	public CustomerOverviewPresenter(Class<V> clazzV, Class<T> clazzT) {
+		super(clazzV,clazzT);		
 	}
 	
 	@SuppressWarnings("unchecked")
@@ -27,7 +19,6 @@ public class CustomerOverviewPresenter<V extends CustomerOverView, T extends Cus
 
 	@Override
 	protected void openDetailview(T persistObject) {
-		//Notification.show("Opening customer: "+persistObject.getLastName());
 		CustomerDetailPresenter<CustomerDetailview, Customer> customerDetailPresenter=new CustomerDetailPresenter<CustomerDetailview, Customer>(CustomerDetailview.class,Customer.class,persistObject,this);
 		customerDetailPresenter.show();
 	}
